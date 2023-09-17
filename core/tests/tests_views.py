22e9c50b867097b7a8fd98849185e0f1ed648316
Luigi Perkoski <celluigipplc@gmail.com>
2023-09-17 13:57:11 -0300

    criando testes pro index

--- core/tests/tests_views.py
+++ core/tests/tests_views.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+    def setUp(self):
+        self.url = 'http://localhost:8000/'
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200) 
+    
+    def test_template_usado(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'core/pages/index.html')
